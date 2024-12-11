27b8af1eb20123a4f4ff1e5081d4c09c35b3eec5
shirakeynan <shirush2002@gmail.com>
2024-12-11 19:59:43 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  file = open(path)
+  rows = file.readlines()
+  file.close()
+  milon = {}
+  for row in rows :
+    row = row.strip().split('\t')
+    codon = row[0]
+    amino_acids = row[2]
+    milon[codon] = amino_acids 
+  return milon
+
 
 
